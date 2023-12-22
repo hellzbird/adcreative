a62db328595aa1933ecd7eadba2377da42c14a36
hellzbird <130483231+hellzbird@users.noreply.github.com>
2023-12-22 11:36:34 +0300

    adcreative_app.py

--- adcreative_app.py
+++ adcreative_app.py
@@ -14,7 +14,7 @@ def generate_image(url,color):
     pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
     image = download_image(url)
     prompt = f"create a similar image, change the object color to {color}"
-    images = pipe(prompt, image=image, num_inference_steps=20, image_guidance_scale=1).images
+    images = pipe(prompt, image=image, num_inference_steps=10, image_guidance_scale=1).images
     generated_image = images[0].save("generated_image.jpg")
     return images[0]
 
@@ -84,7 +84,7 @@ def main():
         
         
     #st.image(generated_image,caption="Generated Image.", use_column_width=True)
-    uploaded_logo = st.file_uploader("Upload the logo", type="jpg")
+    
     #prompt_input = st.text_input("Enter the prompt:")
     #st.write("You entered:", prompt_input)
     punchline_input = st.text_input("Enter the punchline:")
@@ -96,6 +96,7 @@ def main():
     button_color_input = st.text_input("Enter the text and button color:")
     st.write("You entered:",button_color_input)
     
+    uploaded_logo = st.file_uploader("Upload the logo", type="jpg")
     if  button_color_input is not None and uploaded_logo is not None and button_input is not None and punchline_input is not None:
         frame_size = (600, 600)
         logo_data = uploaded_logo.getvalue()
